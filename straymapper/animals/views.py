dd1113dc9c4ebe87ea044d5a58cec5a315d87949
Aurelio Tinio <aurelio@tinio.org>
2012-06-04 14:19:12 -0700

    adds sample hello world view

--- straymapper/animals/views.py
+++ straymapper/animals/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def index(request, template_name='animals/index.html'):
+  context = {}
+
+  return render_to_response(template_name, context,
+      context_instance=RequestContext(request))
--- straymapper/straymapper/urls.py
+++ straymapper/straymapper/urls.py
@@ -9,9 +9,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'straymapper.views.home', name='home'),
     # url(r'^straymapper/', include('straymapper.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^animals/', include('animals.urls'))
 )
