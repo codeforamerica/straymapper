78521b015c13ff4c01a25bb0ad802b6e66990be4
Aurelio Tinio <aurelio@tinio.org>
2012-06-04 15:07:55 -0700

    adds django-bootstrap-toolkit

--- straymapper/straymapper/settings.py
+++ straymapper/straymapper/settings.py
@@ -126,6 +126,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+
+    'bootstrap_toolkit',
     'animals',
 )
 
