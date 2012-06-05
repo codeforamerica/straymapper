bf8aa1c2933f644833b17abd2e453658bd3b10d5
Aurelio Tinio <aurelio@tinio.org>
2012-06-04 17:01:13 -0700

    displays animals as markers

--- straymapper/animals/views.py
+++ straymapper/animals/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from animals.models import Animal
+
 def index(request, template_name='animals/index.html'):
   context = {}
+
+  context['animal_list'] = Animal.objects.all()
 
   return render_to_response(template_name, context,
       context_instance=RequestContext(request))
