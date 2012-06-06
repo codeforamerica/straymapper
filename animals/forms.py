8f9b17aa8787dd01ed6105648a0c288c6b131100
Aurelio Tinio <aurelio@tinio.org>
2012-06-06 11:42:11 -0700

    adds filter by intake condition

--- animals/forms.py
+++ animals/forms.py
@@ -3,18 +3,31 @@ from datetime import date
 from django import forms
 
 class AnimalSearchForm(forms.Form):
-    intake_date = forms.DateField(required=False)
     animal_type_choices = (
-      ('', '----'),
-      ('CAT', 'Cat'),
-      ('DOG', 'Dog'),
+        ('', '----'),
+        ('CAT', 'Cat'),
+        ('DOG', 'Dog'),
     )
     animal_type = forms.ChoiceField(required=False, choices=animal_type_choices)
     sex_choices = (
-      ('', '----'),
-      ('M', 'Male'),
-      ('F', 'Female'),
-      ('S', 'Spayed'),
-      ('N', 'Neutered'),
+        ('', '----'),
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('S', 'Spayed'),
+        ('N', 'Neutered'),
     )
     sex = forms.ChoiceField(required=False, choices=sex_choices)
+    intake_date = forms.DateField(required=False)
+    intake_condition_choices = (
+        ('', '----'),
+        ('NORMAL', 'Normal'),
+        ('INJURED', 'Injured'),
+        ('NURSING', 'Nursing'),
+        ('FERAL', 'Feral'),
+        ('AGED', 'Aged'),
+        ('SICK', 'Sick'),
+        ('PREGNANT', 'Pregnant'),
+        ('OTHER', 'Other'),
+    )
+    intake_condition = forms.ChoiceField(required=False, choices=intake_condition_choices)
+
--- animals/views.py
+++ animals/views.py
@@ -11,15 +11,18 @@ def index(request, template_name='animals/index.html'):
         form = AnimalSearchForm(request.POST)
         animal_list = Animal.objects.all()
         if form.is_valid():
-          intake_date = form.cleaned_data['intake_date']
-          if intake_date:
-            animal_list = animal_list.filter(intake_date=intake_date)
-          animal_type = form.cleaned_data['animal_type']
-          if animal_type:
-            animal_list = animal_list.filter(animal_type=animal_type)
-          sex = form.cleaned_data['sex']
-          if sex:
-            animal_list = animal_list.filter(sex=sex)
+            intake_condition = form.cleaned_data['intake_condition']
+            if intake_condition:
+                animal_list = animal_list.filter(intake_condition=intake_condition)
+            intake_date = form.cleaned_data['intake_date']
+            if intake_date:
+                animal_list = animal_list.filter(intake_date=intake_date)
+            animal_type = form.cleaned_data['animal_type']
+            if animal_type:
+                animal_list = animal_list.filter(animal_type=animal_type)
+            sex = form.cleaned_data['sex']
+            if sex:
+                animal_list = animal_list.filter(sex=sex)
     else:
         form = AnimalSearchForm()
         animal_list = Animal.objects.all()
