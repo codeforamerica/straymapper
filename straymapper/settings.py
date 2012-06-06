6da99b052efae43a905e9dceac656aa28ac1d4d7
Aurelio Tinio <aurelio@tinio.org>
2012-06-06 09:43:59 -0700

    adds use of jquery ui datepicker

--- straymapper/settings.py
+++ straymapper/settings.py
@@ -73,6 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    map_path('public'),
 )
 
 # List of finder classes that know how to find static files in
