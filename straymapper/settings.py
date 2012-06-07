2fe71003073673293fcc111ceb4d41deeae75dc7
Aurelio Tinio <aurelio@tinio.org>
2012-06-06 17:44:35 -0700

    specifies full path to db file

--- straymapper/settings.py
+++ straymapper/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.spatialite', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'straymapperdb',                      # Or path to database file if using sqlite3.
+        'NAME': map_path('straymapperdb'), # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
