3819374df1754d67ea5c1ac2b760964ab1948cf6
Aurelio Tinio <aurelio@tinio.org>
2012-06-13 16:12:48 -0700

    #8 pep8 animals/views.py

--- animals/views.py
+++ animals/views.py
@@ -4,30 +4,31 @@ from django.template import RequestContext
 from animals.models import Animal
 from animals.forms import AnimalSearchForm
 
+
 def index(request, template_name='animals/index.html'):
     context = {}
 
     if request.method == 'POST':
         form = AnimalSearchForm(request.POST)
-        animal_list = Animal.objects.all()
+        alist = Animal.objects.all()
         if form.is_valid():
             intake_condition = form.cleaned_data['intake_condition']
             if intake_condition:
-                animal_list = animal_list.filter(intake_condition=intake_condition)
+                alist = animal_list.filter(intake_condition=intake_condition)
             intake_date = form.cleaned_data['intake_date']
             if intake_date:
-                animal_list = animal_list.filter(intake_date=intake_date)
+                alist = animal_list.filter(intake_date=intake_date)
             animal_type = form.cleaned_data['animal_type']
             if animal_type:
-                animal_list = animal_list.filter(animal_type=animal_type)
+                alist = animal_list.filter(animal_type=animal_type)
             sex = form.cleaned_data['sex']
             if sex:
-                animal_list = animal_list.filter(sex=sex)
+                alist = animal_list.filter(sex=sex)
     else:
         form = AnimalSearchForm()
-        animal_list = Animal.objects.all()
+        alist = Animal.objects.all()
 
     context['form'] = form
-    context['animal_list'] = animal_list
+    context['alist'] = animal_list
     return render_to_response(template_name, context,
         context_instance=RequestContext(request))
