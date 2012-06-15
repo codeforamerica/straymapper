298d8c9cc76cf5263b36ec35b7500a0654d28241
Michelle Lee <michelle.tai.lee@gmail.com>
2012-06-15 16:53:21 -0400

    changes views.py to show the most recent animals

--- animals/views.py
+++ animals/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from datetime import datetime, date, timedelta
 
 from animals.models import Animal
 from animals.forms import AnimalSearchForm
@@ -7,10 +8,14 @@ from animals.forms import AnimalSearchForm
 
 def index(request, template_name='animals/index.html'):
     context = {}
+    #The following line is commented out because we do not have data from 2 weeks ago
+    #startdate = datetime.today() - timedelta(days=14)
+    startdate = datetime.today() - timedelta(days=24)
 
     if request.method == 'POST':
         form = AnimalSearchForm(request.POST)
-        alist = Animal.objects.all()
+        alist = Animal.objects.all()     
+
         if form.is_valid():
             intake_condition = form.cleaned_data['intake_condition']
             if intake_condition:
@@ -24,11 +29,14 @@ def index(request, template_name='animals/index.html'):
             sex = form.cleaned_data['sex']
             if sex:
                 alist = alist.filter(sex=sex)
+            alist= alist[:5]
     else:
         form = AnimalSearchForm()
-        alist = Animal.objects.all()
+        alist_all = Animal.objects.all()
+        alist= alist_all.filter(intake_date__gte=startdate)
 
     context['form'] = form
     context['alist'] = alist
+    
     return render_to_response(template_name, context,
         context_instance=RequestContext(request))
