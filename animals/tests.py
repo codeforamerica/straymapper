f3331fdb1e1e0780b2f4e7e55833c9fea442b2e3
Aurelio Tinio <aurelio@tinio.org>
2012-06-19 12:20:51 -0700

    add basic test for animals index view

--- animals/tests.py
+++ animals/tests.py
@@ -1,16 +1,11 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class AnimalsViewsTestCase(TestCase):
+    def test_index(self):
+        resp = self.client.get('/animals/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('form' in resp.context)
+        self.assertTrue('alist' in resp.context)
+        self.assertTrue('results_count' in resp.context)
+
