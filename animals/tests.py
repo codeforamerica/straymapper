2a83bb654d625a1f5d879c00f138f77984c14ce5
Aurelio Tinio <aurelio@tinio.org>
2012-06-19 12:33:57 -0700

    add simple test for bug issue #15

--- animals/tests.py
+++ animals/tests.py
@@ -1,11 +1,16 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 
 class AnimalsViewsTestCase(TestCase):
     def test_index(self):
-        resp = self.client.get('/animals/')
+        resp = self.client.get(reverse('animals_index'))
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('form' in resp.context)
         self.assertTrue('alist' in resp.context)
         self.assertTrue('results_count' in resp.context)
+
+    def test_cat_search(self):
+        resp = self.client.post(reverse('animals_index'), {'animal_type': 'CAT'})
+        self.assertEqual(resp.status_code, 200)
 
