e33ce1d121bd9283209df0197af7fb1eb1458a49
Aurelio Tinio <aurelio@tinio.org>
2012-06-19 12:48:24 -0700

    add test for bug issue #16

--- animals/tests.py
+++ animals/tests.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from animals.models import Animal
 
 class AnimalsViewsTestCase(TestCase):
     def test_index(self):
@@ -14,3 +17,19 @@ class AnimalsViewsTestCase(TestCase):
         resp = self.client.post(reverse('animals_index'), {'animal_type': 'CAT'})
         self.assertEqual(resp.status_code, 200)
 
+    def test_markers_displayed(self):
+        Animal.objects.create(
+            intake_date=datetime.today(),
+            location="9411 BARNESDALLE AUSTIN TX",
+            intake_condition='NORMAL',
+            animal_type='CAT',
+            sex='M',
+            age=4763,
+            color='TORBIE',
+            breed='DOMESTIC SH',
+            intake_total=2,
+            animal_id='A163112',
+            geometry='POINT (-97.7932349999999957 30.1910789999999984)'
+        )
+        resp = self.client.get(reverse('animals_index'))
+        self.assertContains(resp, "map.addMarker", status_code=200)
