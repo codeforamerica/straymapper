661bfb022d65fdc3c906d2d8a27286b4a52ac287
Aurelio Tinio <aurelio@tinio.org>
2012-06-21 16:10:07 -0700

    add more search tests

--- animals/tests.py
+++ animals/tests.py
@@ -15,9 +15,29 @@ class AnimalsViewsTestCase(TestCase):
         self.assertTrue('alist' in resp.context)
         self.assertTrue('results_count' in resp.context)
 
-    def test_cat_search(self):
+    def test_type_search(self):
         resp = self.client.post(reverse('animals_index'), {'animal_type': 'CAT'})
-        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, "5/31/2012")
+        self.assertContains(resp, "cypress")
+        self.assertContains(resp, "brn tabby domestic sh", status_code=200)
+
+    def test_sex_search(self):
+        resp = self.client.post(reverse('animals_index'), {'sex': 'M'})
+        self.assertContains(resp, "5/30/2012")
+        self.assertContains(resp, "beagle")
+        self.assertContains(resp, "tricolor beagle", status_code=200)
+
+    def test_date_search(self):
+        resp = self.client.post(reverse('animals_index'), {'intake_date': '2012-06-04'})
+        self.assertContains(resp, "6/4/2012")
+        self.assertContains(resp, "chalupa")
+        self.assertContains(resp, "white rat terrier", status_code=200)
+
+    def test_condition_search(self):
+        resp = self.client.post(reverse('animals_index'), {'intake_condition': 'INJURED'})
+        self.assertContains(resp, "5/30/2012")
+        self.assertContains(resp, "bo jangles")
+        self.assertContains(resp, "white catahoula", status_code=200)
 
     def test_markers_displayed(self):
         resp = self.client.get(reverse('animals_index'))
