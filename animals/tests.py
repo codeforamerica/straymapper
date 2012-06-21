64e5a0fa0f6cbaf1b1411d6e54fe98fcb9fa8de7
Aurelio Tinio <aurelio@tinio.org>
2012-06-21 15:42:49 -0700

    use fixtures for test

--- animals/tests.py
+++ animals/tests.py
@@ -6,6 +6,8 @@ from django.test import TestCase
 from animals.models import Animal
 
 class AnimalsViewsTestCase(TestCase):
+    fixtures = ['animals_testdata.json']
+
     def test_index(self):
         resp = self.client.get(reverse('animals_index'))
         self.assertEqual(resp.status_code, 200)
@@ -18,18 +20,5 @@ class AnimalsViewsTestCase(TestCase):
         self.assertEqual(resp.status_code, 200)
 
     def test_markers_displayed(self):
-        Animal.objects.create(
-            intake_date=datetime.today(),
-            location="9411 BARNESDALLE AUSTIN TX",
-            intake_condition='NORMAL',
-            animal_type='CAT',
-            sex='M',
-            age=4763,
-            name='Pepper',
-            description='gray schnauzer min',
-            intake_total=2,
-            animal_id='A163112',
-            geometry='POINT (-97.7932349999999957 30.1910789999999984)'
-        )
         resp = self.client.get(reverse('animals_index'))
-        self.assertContains(resp, "map.addMarker", status_code=200)
+        self.assertContains(resp, "map.addMarker", count=100, status_code=200)
