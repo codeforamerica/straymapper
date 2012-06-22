f5a5da7539afb8aba55473dcb9a1007b0968bd50
Aurelio Tinio <aurelio@tinio.org>
2012-06-21 17:48:28 -0700

    pep8 animals/tests.py

--- animals/tests.py
+++ animals/tests.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 
 from animals.models import Animal
 
+
 class AnimalsViewsTestCase(TestCase):
     fixtures = ['animals_testdata.json']
 
@@ -16,7 +17,8 @@ class AnimalsViewsTestCase(TestCase):
         self.assertTrue('results_count' in resp.context)
 
     def test_type_search(self):
-        resp = self.client.post(reverse('animals_index'), {'animal_type': 'CAT'})
+        resp = self.client.post(reverse('animals_index'),
+            {'animal_type': 'CAT'})
         self.assertContains(resp, "5/31/2012")
         self.assertContains(resp, "cypress")
         self.assertContains(resp, "brn tabby domestic sh", status_code=200)
@@ -28,13 +30,15 @@ class AnimalsViewsTestCase(TestCase):
         self.assertContains(resp, "tricolor beagle", status_code=200)
 
     def test_date_search(self):
-        resp = self.client.post(reverse('animals_index'), {'intake_date': '2012-06-04'})
+        resp = self.client.post(reverse('animals_index'),
+            {'intake_date': '2012-06-04'})
         self.assertContains(resp, "6/4/2012")
         self.assertContains(resp, "chalupa")
         self.assertContains(resp, "white rat terrier", status_code=200)
 
     def test_condition_search(self):
-        resp = self.client.post(reverse('animals_index'), {'intake_condition': 'INJURED'})
+        resp = self.client.post(reverse('animals_index'),
+            {'intake_condition': 'INJURED'})
         self.assertContains(resp, "5/30/2012")
         self.assertContains(resp, "bo jangles")
         self.assertContains(resp, "white catahoula", status_code=200)
