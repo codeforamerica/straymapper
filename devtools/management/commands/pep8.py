85da1a9bc8907da7ec36aff8fae42f6a6bb6eaf9
Aurelio Tinio <aurelio@tinio.org>
2012-06-25 12:56:49 -0700

    make pep8 devtools path generic

--- devtools/management/commands/pep8.py
+++ devtools/management/commands/pep8.py
@@ -1,4 +1,6 @@
-from django.core.management.base import BaseCommand, CommandError
+import os
+
+from django.core.management.base import BaseCommand
 
 from fabric.api import local
 
@@ -7,5 +9,7 @@ class Command(BaseCommand):
     help = 'Runs pep8 on the entire project'
 
     def handle(self, *args, **options):
-        local('find ../straymapper -name "*.py" | xargs pep8 --repeat',
-            capture=False)
+        file_path = os.path.dirname(__file__)
+        pep8_cmd = 'find ' + file_path
+        pep8_cmd = pep8_cmd + '/../../.. -name "*.py" | xargs pep8 --repeat'
+        local(pep8_cmd, capture=False)
