b656195aee8e0a206dff9184d529c425dca6a9f9
Aurelio Tinio <aurelio@tinio.org>
2012-07-31 14:41:15 -0700

    fix setttings credentials check

--- straymapper/settings.py
+++ straymapper/settings.py
@@ -32,7 +32,7 @@ DATABASES = {
 }
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY', '') 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '') 
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '') 
 AWS_STORAGE_BUCKET_NAME = 'citypetz'
 AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
