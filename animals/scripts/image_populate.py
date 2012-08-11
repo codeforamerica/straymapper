0d323631c3705603f008a3aa19471dbe0a66f0fc
Aurelio Tinio <aurelio@tinio.org>
2012-08-11 16:46:36 -0700

    pep8 image_populate.py and other cleanup

--- animals/scripts/image_populate.py
+++ animals/scripts/image_populate.py
@@ -1,28 +1,30 @@
-import os 
-import csv 
-from django.core.files.storage import default_storage 
-from django.core.files.images import ImageFile 
+import os
+import csv
+from django.core.files.storage import default_storage
+from django.core.files.images import ImageFile
 
-from animals.models import Animal 
+from animals.models import Animal
 
-def run(): 
-    csv_file = open("%s/../fixtures/dataset_072012.csv" % os.path.dirname(__file__)) 
-    contents = csv.reader(csv_file, dialect='excel', delimiter=',') 
-    header = contents.next() 
-    
-    for row in contents: 
-        animal_id = row[3] 
-        s = '/home/michelle/work/CfA/Images/' + animal_id + '.jpg' 
 
-        try: 
-            with open(s, 'rb') as photo: 
-                if Animal.objects.filter(animal_id=animal_id): 
+def run():
+    script_path = os.path.dirname(__file__)
+    csv_file = open("%s/../fixtures/dataset_072012.csv" % script_path)
+    contents = csv.reader(csv_file, dialect='excel', delimiter=',')
+    header = contents.next()
+
+    for row in contents:
+        animal_id = row[3]
+        s = '/home/michelle/work/CfA/Images/' + animal_id + '.jpg'
+
+        try:
+            with open(s, 'rb') as photo:
+                if Animal.objects.filter(animal_id=animal_id):
                     a = Animal.objects.filter(animal_id=animal_id)[0]
                     imagephoto = ImageFile(photo)
                     a.photo.save(s, imagephoto, save=True)
-                    print "appended photo to animal %s" %animal_id 
-                else: 
-                    print "have photo but missing record for %s" %animal_id 
+                    print "appended photo to animal %s" % animal_id
+                else:
+                    print "have photo but missing record for %s" % animal_id
 
-        except IOError: 
+        except IOError:
             print "missing photo for " + animal_id
