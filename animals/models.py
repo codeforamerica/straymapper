c73a2805c16e6ffad58bc49f366e38d23362d820
Aurelio Tinio <aurelio@tinio.org>
2012-08-12 03:43:52 -0700

    cleanup models.py

--- animals/models.py
+++ animals/models.py
@@ -4,29 +4,30 @@ from imagekit.processors import ResizeToFill, Adjust
 
 
 class Animal(models.Model):
-    intake_date = models.DateField('Intake Date')
-    location = models.CharField('Location', max_length=255)
-    intake_condition = models.CharField('Intake Condition', max_length=255)
     TYPE_CHOICES = (
         (u'PUPPY', u'Puppy'),
         (u'KITTEN', u'Kitten'),
         (u'DOG', u'Dog'),
         (u'CAT', u'Cat'),
     )
-    animal_type = models.CharField('Animal Type', max_length=255,
-        choices=TYPE_CHOICES)
+
     GENDER_CHOICES = (
         (u'M', u'Male'),
         (u'F', u'Female'),
         (u'U', u'Unknown'),
     )
-    sex = models.CharField('Sex', max_length=2, choices=GENDER_CHOICES)
+
+    intake_date = models.DateField()
+    location = models.CharField(max_length=255)
+    intake_condition = models.CharField(max_length=255)
+    animal_type = models.CharField(max_length=255, choices=TYPE_CHOICES)
+    sex = models.CharField(max_length=2, choices=GENDER_CHOICES)
     spayed = models.BooleanField('Spayed or Neutered', default=False)
     age = models.IntegerField('Age in Days')
     name = models.CharField(max_length=255, blank=True)
     description = models.CharField(max_length=255)
-    intake_total = models.IntegerField('Intake Total')
-    animal_id = models.CharField('Animal ID', max_length=255)
+    intake_total = models.IntegerField()
+    animal_id = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='photo', blank=True, null=True)
     thumbnail = ImageSpecField([Adjust(contrast=1.2, sharpness=1.1),
         ResizeToFill(120, 90)], image_field='photo', format="JPEG",
