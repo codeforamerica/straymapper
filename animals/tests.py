63a135080aff7346c3b6c499a34648dd6149c2dd
Aurelio Tinio <aurelio@tinio.org>
2012-08-13 10:55:40 -0700

    fix paginated type & condition search tests

--- animals/tests.py
+++ animals/tests.py
@@ -27,8 +27,17 @@ class AnimalsViewsTestCase(TestCase):
         self.assertContains(resp, "5/31/2012")
         self.assertContains(resp, "brn tabby domestic sh", status_code=200)
 
+    def test_date_search(self):
+        resp = self.client.post(reverse('animals_index'),
+            {'intake_date_start': '2012-06-04',
+             'intake_date_end': '2012-06-04'})
+        self.assertContains(resp, "6/4/2012")
+        self.assertContains(resp, "chalupa")
+        self.assertContains(resp, "white rat terrier", status_code=200)
+
     def test_paginated_type_search(self):
-        self.client.post(reverse('animals_index'), {'animal_type': 'CAT'})
+        self.client.post(reverse('animals_index'), {'animal_type': 'CAT',
+            'intake_date_start': '2012-05-01'})
         resp = self.client.get('%s?page=2' % reverse('animals_index'))
         self.assertContains(resp, "6/2/2012", count=5)
         self.assertContains(resp, "calico domestic sh", status_code=200)
@@ -39,17 +48,10 @@ class AnimalsViewsTestCase(TestCase):
         self.assertContains(resp, "beagle")
         self.assertContains(resp, "tricolor beagle", status_code=200)
 
-    def test_date_search(self):
-        resp = self.client.post(reverse('animals_index'),
-            {'intake_date_start': '2012-06-04',
-             'intake_date_end': '2012-06-04'})
-        self.assertContains(resp, "6/4/2012")
-        self.assertContains(resp, "chalupa")
-        self.assertContains(resp, "white rat terrier", status_code=200)
-
     def test_condition_search(self):
-        resp = self.client.post(reverse('animals_index'),
-            {'intake_condition': 'INJURED'})
+        resp = self.client.post(reverse('animals_index'),{
+            'intake_condition': 'INJURED',
+            'intake_date_start': '2012-05-01'})
         self.assertContains(resp, "5/30/2012")
         self.assertContains(resp, "bo jangles")
         self.assertContains(resp, "white catahoula", status_code=200)
