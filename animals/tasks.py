fda49817251e4ce28b5351c87ca353852be6c318
Aurelio Tinio <aurelio@tinio.org>
2012-08-22 16:41:40 -0500

    move populate process into a task

--- animals/tasks.py
+++ animals/tasks.py
@@ -1,5 +1,5 @@
 from celery import task
 
 @task()
-def populate(message):
-    return message + ' done'
+def populate(animal_id):
+    return 'processing %s' % animal_id
--- animals/views.py
+++ animals/views.py
@@ -30,7 +30,6 @@ def utf_8_encoder(unicode_csv_data):
 
 @csrf_exempt
 def process_data(request):
-    populate.delay('populate task')
     if request.method == 'POST':
         sender = request.POST.get('sender')
         recipient = request.POST.get('recipient')
@@ -44,35 +43,7 @@ def process_data(request):
             g = geocoders.Google('AIzaSyAZoNPSlRTETltbmJvgYYqol0SLAVBgKs')
             for row in contents:
                 animal_id = row[3]
-                if not Animal.objects.filter(animal_id=animal_id).exists():
-                    location = row[1]
-                    location_found = True
-                    try:
-                        (place, point) = g.geocode(location)
-                    except:
-                        location_found = False
-                    if location_found:
-                        a = Animal()
-                        a.animal_id = animal_id
-                        intake_date = row[0]
-                        dt = datetime.strptime(intake_date.strip(), "%m/%d/%y")
-                        a.intake_date = date(year=dt.year, month=dt.month,
-                            day=dt.day)
-                        a.location = location
-                        a.intake_condition = row[2]
-                        a.animal_type = row[4]
-                        sex = {'UNKNOWN': 'U',
-                                'MALE': 'M',
-                                'FEMALE': 'F'}
-                        a.sex = sex[row[5]]
-                        a.spayed = True if row[6] == 'YES' else False
-                        a.name = row[7]
-                        a.age = int(float(row[8].replace(',', '')))
-                        a.description = row[9]
-                        a.intake_total = 1
-                        a.geometry = "POINT (%s %s)" % (point[1], point[0])
-                        a.photo = ''
-                        a.save()
+                populate.delay(animal_id)
     return HttpResponse('cool')
 
 
