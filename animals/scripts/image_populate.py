3d65137ee317ab885dea261602bc44dfb1af1e36
Aurelio Tinio <aurelio@tinio.org>
2012-08-25 13:33:13 -0500

    process updated images

--- animals/scripts/image_populate.py
+++ animals/scripts/image_populate.py
@@ -6,12 +6,13 @@ import requests
 
 from django.core.files.storage import default_storage
 from django.core.files.images import ImageFile
+from django.db.models import Q
 
 from animals.models import Animal
 
 
 def run():
-    for a in Animal.objects.filter(photo=u''):
+    for a in Animal.objects.filter(Q(photo=u'') | Q(photo_updated=True)):
         print a.animal_id
         filename = a.animal_id + '.jpg'
         s = 'https://citypetz.s3.amazonaws.com/images/' + filename
@@ -27,6 +28,8 @@ def run():
                     print photo
                     imagephoto = ImageFile(photo)
                     a.photo.save(filename, imagephoto, save=True)
+                    a.photo_updated=False
+                    a.save()
                     print "appended photo to animal"
                     thumb = a.thumbnail
                     thumb_url = a.thumbnail.url
