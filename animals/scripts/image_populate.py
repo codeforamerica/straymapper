96613e23197c0c4957c2e257a7df902df5ac5d6a
Aurelio Tinio <aurelio@tinio.org>
2012-08-25 09:34:25 -0500

    update image_populate.py

--- animals/scripts/image_populate.py
+++ animals/scripts/image_populate.py
@@ -2,6 +2,8 @@ import csv
 import os
 import urllib
 
+import requests
+
 from django.core.files.storage import default_storage
 from django.core.files.images import ImageFile
 
@@ -15,21 +17,22 @@ def run():
         s = 'https://citypetz.s3.amazonaws.com/images/' + filename
         print s
 
-        try:
+        r = requests.head(s)
+        if r.status_code == 200:
             temp_photo = urllib.urlretrieve(s, '/tmp/%s' % filename)
-        except:
-            pass
 
-        try:
-            if temp_photo:
-                photo = open(temp_photo[0], 'rb')
-                print photo
-                imagephoto = ImageFile(photo)
-                a.photo.save(filename, imagephoto, save=True)
-                print "appended photo to animal"
-                thumb = a.thumbnail
-                thumb_url = a.thumbnail.url
-            else:
-                print "have record but missing photo for %s" % a.animal_id
-        except IOError:
+            try:
+                if temp_photo:
+                    photo = open(temp_photo[0], 'rb')
+                    print photo
+                    imagephoto = ImageFile(photo)
+                    a.photo.save(filename, imagephoto, save=True)
+                    print "appended photo to animal"
+                    thumb = a.thumbnail
+                    thumb_url = a.thumbnail.url
+                else:
+                    print "have record but missing photo for %s" % a.animal_id
+            except IOError:
+                print "missing photo for " + a.animal_id
+        else:
             print "missing photo for " + a.animal_id
