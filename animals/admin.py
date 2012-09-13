11ff031f18a5af9bf4132513fa1508b4c0a80e00
Aurelio Tinio <aurelio@tinio.org>
2012-09-13 13:10:30 -0700

    Yep, still breaking so reverting "#56 check if OSMGeoAdmin still fails travis-ci now that on PostgreSQL backend used"
    This reverts commit 6f1dfbd4fcf4af39c33e37ed67249aba55d74f1d.

--- animals/admin.py
+++ animals/admin.py
@@ -3,7 +3,7 @@ from django.contrib.gis import admin
 from animals.models import Animal
 
 
-class AnimalAdmin(admin.OSMGeoAdmin):
+class AnimalAdmin(admin.GeoModelAdmin):
     list_display = ('animal_id', 'intake_date', 'animal_type', 'sex')
     search_fields = ('animal_id', 'name')
     ordering = ('-intake_date',)
