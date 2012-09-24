3ea88e6343c2cc0700fd8104427b465bedf76ebb
Aurelio Tinio <aurelio@tinio.org>
2012-09-24 11:34:05 -0700

    closes #87 add stub template for about page

--- straymapper/urls.py
+++ straymapper/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -6,6 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    (r'^about/$', direct_to_template, {'template': 'about.html'}),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
